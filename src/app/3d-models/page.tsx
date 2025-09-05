49f5b7818e5643810ca58d50cefe1a05327233cd
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-09-06 01:08:05 +1000

    next form

--- src/app/3d-models/page.tsx
+++ src/app/3d-models/page.tsx
@@ -1,6 +1,7 @@
 import {getModels} from '@/app/lib/models';
 import ModelsGrid from '@/app/components/ModelsGrid';
 import { ModelPageProps } from '../types';
+import Form from "next/form";
 
 export default async function ModelsPage({searchParams}: ModelPageProps) {
 
@@ -15,7 +16,7 @@ export default async function ModelsPage({searchParams}: ModelPageProps) {
     : models;
   return (
     <>
-      <form className="w-full px-5 md:px-0 md:max-w-xl">
+      <Form action="/3d-models" className="w-full px-5 md:px-0 md:max-w-xl">
         <label htmlFor="search-3d-models" className="sr-only">Search 3D Models</label>
         <input 
           type="text"
@@ -25,7 +26,7 @@ export default async function ModelsPage({searchParams}: ModelPageProps) {
           defaultValue={query}
           className="w-full py-3 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none"
         />
-      </form>
+      </Form>
       <ModelsGrid title="3D Models" models={filteredModels} />
     </>
   );
